6a4e2d19cc4988969ca0a78ef88887f0855ad0a3
truth1024 <truth1024@hotmail.com>
2014-03-26 23:59:27 +0800

    添加用户功能

---
+++ amwayfc/src/main/java/com/richmobi/amwayfc/action/UserAction.java
@@ -0,0 +1,55 @@
+package com.richmobi.amwayfc.action;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.richmobi.amwayfc.domain.User;
+import com.richmobi.amwayfc.service.UserService;
+
+public class UserAction extends BasicAction {
+
+	private static final long serialVersionUID = 1617158807956202883L;
+	private static final Logger log = LoggerFactory.getLogger(UserAction.class);
+	
+	private int status;
+	private String tip;
+	private User user;
+	private List<User> us;
+	
+	@Autowired
+	UserService userService;
+
+	public String users(){
+		String logincode = getSessionLogin().getLogincode();
+		us = userService.getUsersByLogincode(logincode);
+		status = 200;
+		return SUCCESS;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getTip() {
+		return tip;
+	}
+	public void setTip(String tip) {
+		this.tip = tip;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public List<User> getUs() {
+		return us;
+	}
+	public void setUs(List<User> us) {
+		this.us = us;
+	}
+}
---
+++ amwayfc/src/main/webapp/web/js/step2.js
@@ -0,0 +1,22 @@
+$(function(){
+	//获取登录编码下的用户信息列表
+	get_users();
+});
+
+//获取用户信息函数
+function get_users(){
+	var url = url_basic+'user_users';
+	$.post(url,get_users_callback);
+};
+
+//获取用户信息回调函数
+function get_users_callback(data){
+	var status = data.status;
+	if(status !== 200){
+		alert(data.tip);
+	}else{
+		if(data.us != undefined && data.us.length !== 0){
+			$('.contentMember').append($(template.render('users',data)));
+		}
+	}
+};
